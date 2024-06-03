1494030baf5b048408d92657035e8dafbb5e46f9
jean <jean.antoine.michelet@gmail.com>
2024-06-03 16:17:51 +0200

    fix: swagger tag Example in /example

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,6 +1,6 @@
 export default async function (fastify, opts) {
   // Prefixed with /example by the autoloader
-  fastify.get('/', { schema: { tags: ['Home'] } }, async function (request, reply) {
+  fastify.get('/', { schema: { tags: ['Example'] } }, async function (request, reply) {
     return 'this is an example'
   })
 }
