b4981ed1582ed8298880d40922a068b358735399
jean <jean.antoine.michelet@gmail.com>
2024-06-05 10:22:06 +0200

    fix: run lint fix

--- test/routes/not-found-handler.test.js
+++ test/routes/not-found-handler.test.js
@@ -3,13 +3,13 @@ import assert from 'node:assert'
 import { build } from '../helper.js'
 
 test('root not found handler', async (t) => {
-  const app = await build(t);
+  const app = await build(t)
 
   const res = await app.inject({
     method: 'GET',
     url: '/this-route-does-not-exist'
-  });
+  })
 
-  assert.strictEqual(res.statusCode, 404);
-  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Not Found' });
-});
+  assert.strictEqual(res.statusCode, 404)
+  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Not Found' })
+})
