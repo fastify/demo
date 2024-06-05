d48b86cef1c52c6bb1e8da117e82c21ec71da396
jean <jean.antoine.michelet@gmail.com>
2024-06-05 09:24:25 +0200

    test: root not found handler

---
+++ test/routes/error-handler.test.js
@@ -0,0 +1,1 @@
+// TODO: See fastify-cli to add route throwing and error before ready is called
---
+++ test/routes/not-found-handler.test.js
@@ -0,0 +1,15 @@
+import { test } from 'node:test'
+import assert from 'node:assert'
+import { build } from '../helper.js'
+
+test('root not found handler', async (t) => {
+  const app = await build(t);
+
+  const res = await app.inject({
+    method: 'GET',
+    url: '/this-route-does-not-exist'
+  });
+
+  assert.strictEqual(res.statusCode, 404);
+  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Not Found' });
+});
