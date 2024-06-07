540e60aa367c4743b79758df32440056a3455fa2
jean <jean.antoine.michelet@gmail.com>
2024-06-07 15:47:55 +0200

    fix: remove commented code

--- test/plugins/support.test.ts
+++ test/plugins/support.test.ts
@@ -11,16 +11,3 @@ test("support works standalone", async () => {
   await fastify.ready();
   assert.equal(fastify.someSupport(), "hugs");
 });
-
-// You can also use plugin with opts in fastify v2
-//
-// test('support works standalone', (t) => {
-//   t.plan(2)
-//   const fastify = Fastify()
-//   fastify.register(Support)
-//
-//   fastify.ready((err) => {
-//     t.error(err)
-//     assert.equal(fastify.someSupport(), 'hugs')
-//   })
-// })
