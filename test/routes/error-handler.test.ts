30a7a57dc67ba23d275a65a31b00a18628ca8656
jean <jean.antoine.michelet@gmail.com>
2024-06-07 13:05:02 +0200

    test: error-handler still todo

--- test/routes/error-handler.test.ts
+++ test/routes/error-handler.test.ts
@@ -1,17 +1,1 @@
-import { test } from "node:test";
-import assert from "node:assert";
-import fastify from "fastify";
-import serviceApp from "../../src/app.ts"
-
-test("root not found handler", async (t) => {
-  const app = fastify() ;
-  app.register(serviceApp)
-
-  const res = await app.inject({
-    method: "GET",
-    url: "/this-route-does-not-exist",
-  });
-
-  assert.strictEqual(res.statusCode, 404);
-  assert.deepStrictEqual(JSON.parse(res.payload), { message: "Not Found" });
-});
+// TODO: See fastify-cli to add route throwing and error before ready is called
