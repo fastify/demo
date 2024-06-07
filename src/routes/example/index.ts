e4dce3495b503026c253455a19791904250cced2
jean <jean.antoine.michelet@gmail.com>
2024-06-07 16:55:02 +0200

    test: POST /example and error-handler

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -21,6 +21,28 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       return { message: "This is an example" };
     },
   );
+
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: Type.Object({
+          digit: Type.Number({ minimum: 0, maximum: 9 }),
+        }),
+        response: {
+          200: Type.Object({
+            message: Type.String(),
+          }),
+        },
+        tags: ["Example"],
+      },
+    },
+    async function (req) {
+      const { digit } = req.body;
+
+      return { message: `Here is the digit you sent: ${digit}` };
+    },
+  );
 };
 
 export default plugin;
--- test/routes/home.test.ts
+++ test/routes/home.test.ts
@@ -2,7 +2,7 @@ import { test } from "node:test";
 import assert from "node:assert";
 import { build } from "../helper.js";
 
-test("default root route", async (t) => {
+test("GET /", async (t) => {
   const app = await build(t);
 
   const res = await app.inject({
