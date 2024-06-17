f88fd1c3c81e23d715a92ab072959a47ae33e3c5
jean <jean.antoine.michelet@gmail.com>
2024-06-17 17:48:57 +0200

    refactor: leverage autoConfig callback feature

--- src/plugins/external/under-pressure.ts
+++ src/plugins/external/under-pressure.ts
@@ -2,6 +2,33 @@ import { FastifyInstance } from "fastify";
 import fastifyUnderPressure from "@fastify/under-pressure";
 import fp from "fastify-plugin";
 
+export const autoConfig = (fastify: FastifyInstance) => {
+  return {
+    maxEventLoopDelay: 1000,
+    maxHeapUsedBytes: 100_000_000,
+    maxRssBytes: 1_000_000_000,
+    maxEventLoopUtilization: 0.98,
+    message: "The server is under pressure, retry later!",
+    retryAfter: 50,
+    healthCheck: async () => {
+      let connection;
+      try {
+        connection = await fastify.mysql.getConnection();
+        await connection.query("SELECT 1;");
+        return true;
+        /* c8 ignore start */
+      } catch (err) {
+        fastify.log.error(err, "healthCheck has failed");
+        throw new Error("Database connection is not available");
+      } finally {
+        connection?.release();
+      }
+      /* c8 ignore stop */
+    },
+    healthCheckInterval: 5000
+  }
+}
+
 /**
  * A Fastify plugin for mesuring process load and automatically
  * handle of "Service Unavailable"
@@ -11,33 +38,7 @@ import fp from "fastify-plugin";
  * Video on the topic: Do not thrash the event loop
  * @see https://www.youtube.com/watch?v=VI29mUA8n9w
  */
-export default fp(
-  async function (fastify: FastifyInstance) {
-    fastify.register(fastifyUnderPressure, {
-      maxEventLoopDelay: 1000,
-      maxHeapUsedBytes: 100_000_000,
-      maxRssBytes: 1_000_000_000,
-      maxEventLoopUtilization: 0.98,
-      message: "The server is under pressure, retry later!",
-      retryAfter: 50,
-      healthCheck: async () => {
-        let connection;
-        try {
-          connection = await fastify.mysql.getConnection();
-          await connection.query("SELECT 1;");
-          return true;
-          /* c8 ignore start */
-        } catch (err) {
-          fastify.log.error(err, "healthCheck has failed");
-          throw new Error("Database connection is not available");
-        } finally {
-          connection?.release();
-        }
-        /* c8 ignore stop */
-      },
-      healthCheckInterval: 5000
-    });
-  },
+export default fp(fastifyUnderPressure,
   {
     dependencies: ["mysql"]
   }
