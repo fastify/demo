43136ed5d00966231ed02b7425714db00bf5a249
jean <jean.antoine.michelet@gmail.com>
2024-07-14 08:13:19 +0200

    test: ensure access-control-allow-methods contains the expected methods

--- src/plugins/external/cors.ts
+++ src/plugins/external/cors.ts
@@ -1,12 +1,12 @@
-import cors from "@fastify/cors";
+import cors, { FastifyCorsOptions } from "@fastify/cors";
 
-export const autoConfig = {
-  // Set plugin options here
+export const autoConfig: FastifyCorsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
 
 /**
  * This plugins enables the use of CORS.
  *
- * @see https://github.com/fastify/fastify-cors
+ * @see {@link https://github.com/fastify/fastify-cors}
  */
 export default cors;
---
+++ test/app/cors.test.ts
@@ -0,0 +1,20 @@
+import { it } from "node:test";
+import { build } from "../helper.js";
+import assert from "node:assert";
+
+it("should correctly handle CORS preflight for common methods and reject TRACE", async (t) => {
+    const app = await build(t);
+
+    const res = await app.inject({
+        method: "OPTIONS",
+        url: "/",
+        headers: {
+            "Origin": "http://example.com",
+            "Access-Control-Request-Method": "GET",
+            "Access-Control-Request-Headers": "Content-Type"
+        }
+    });
+
+    assert.strictEqual(res.statusCode, 204);
+    assert.strictEqual(res.headers['access-control-allow-methods'], 'GET, POST, PUT, DELETE');
+});
