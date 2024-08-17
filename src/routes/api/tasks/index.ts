b5ee1d9187a318f672dfee693aa7b3abe56d6c5f
jean <jean.antoine.michelet@gmail.com>
2024-08-17 12:56:58 +0200

    feat: create basic crud

--- src/routes/api/tasks/index.ts
+++ src/routes/api/tasks/index.ts
@@ -2,7 +2,14 @@ import {
   FastifyPluginAsyncTypebox,
   Type
 } from "@fastify/type-provider-typebox";
-import { TaskSchema } from "../../../schemas/tasks.js";
+import {
+  TaskSchema,
+  Task,
+  CreateTaskSchema,
+  UpdateTaskSchema,
+  PatchTaskStatusSchema
+} from "../../../schemas/tasks.js";
+import { FastifyReply } from "fastify";
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.get(
@@ -16,9 +23,157 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       }
     },
     async function () {
-      return [{ id: 1, name: "Do something..." }];
+      const tasks = await fastify.repository.findMany<Task>("tasks");
+
+      return tasks;
+    }
+  );
+
+  fastify.get(
+    "/:id",
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        response: {
+          200: TaskSchema,
+          404: Type.Object({ message: Type.String() })
+        },
+        tags: ["Tasks"]
+      }
+    },
+    async function (request, reply) {
+      const { id } = request.params;
+      const task = await fastify.repository.find<Task>("tasks", { where: { id } });
+
+      if (!task) {
+        return notFound(reply);
+      }
+
+      return task;
+    }
+  );
+
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: CreateTaskSchema,
+        response: {
+          201: {
+            id: Type.Number()
+          }
+        },
+        tags: ["Tasks"]
+      }
+    },
+    async function (request, reply) {
+      const id = await fastify.repository.create("tasks", { data: request.body });
+      reply.code(201);
+
+      return {
+        id
+      };
+    }
+  );
+
+  fastify.put(
+    "/:id",
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        body: UpdateTaskSchema,
+        response: {
+          200: TaskSchema,
+          404: Type.Object({ message: Type.String() })
+        },
+        tags: ["Tasks"]
+      }
+    },
+    async function (request, reply) {
+      const { id } = request.params;
+      const affectedRows = await fastify.repository.update("tasks", {
+        data: request.body,
+        where: { id }
+      });
+
+      if (affectedRows === 0) {
+        return notFound(reply)
+      }
+
+      const task = await fastify.repository.find<Task>("tasks", { where: { id } });
+
+      return task as Task;
+    }
+  );
+
+  fastify.patch(
+    "/:id/status",
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        body: PatchTaskStatusSchema,
+        response: {
+          200: Type.Object({ message: Type.String() }),
+          404: Type.Object({ message: Type.String() })
+        },
+        tags: ["Tasks"]
+      }
+    },
+    async function (request, reply) {
+      const { id } = request.params;
+      const { status } = request.body;
+
+      const affectedRows = await fastify.repository.update("tasks", {
+        data: { status },
+        where: { id }
+      });
+
+      if (affectedRows === 0) {
+        return notFound(reply)
+      }
+
+      return {
+        message: "Status changed"
+      };
+    }
+  );
+
+  fastify.delete(
+    "/:id",
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        response: {
+          204: Type.Null(),
+          404: Type.Object({ message: Type.String() })
+        },
+        tags: ["Tasks"]
+      }
+    },
+    async function (request, reply) {
+      const { id } = request.params;
+      const affectedRows = await fastify.repository.delete("tasks", { id });
+
+      if (affectedRows === 0) {
+        return notFound(reply)
+      }
+
+      reply.code(204).send(null);
     }
   );
 };
+
+function notFound(reply: FastifyReply) {
+  reply.code(404)
+  return { message: "Task not found" }
+}
 
 export default plugin;
--- test/helper.ts
+++ test/helper.ts
@@ -1,21 +1,31 @@
 // This file contains code that we reuse
 // between our tests.
 
-import { InjectOptions } from "fastify";
+import { FastifyInstance, InjectOptions } from "fastify";
 import { build as buildApplication } from "fastify-cli/helper.js";
 import path from "node:path";
 import { TestContext } from "node:test";
+
+declare module "fastify" {
+  interface FastifyInstance {
+    login: typeof login;
+    injectWithLogin: typeof injectWithLogin
+  }
+}
+
 
 const AppPath = path.join(import.meta.dirname, "../src/app.ts");
 
 // Fill in this config with all the configurations
 // needed for testing the application
 export function config() {
-  return {};
+  return {
+    skipOverride: "true" // Register our application with fastify-plugin
+  };
 }
 
 // We will create different users with different roles
-async function login(username: string) {
+async function login(this: FastifyInstance, username: string) {
   const res = await this.inject({
     method: "POST",
     url: "/api/auth/login",
@@ -28,6 +38,15 @@ async function login(username: string) {
   return JSON.parse(res.payload).token;
 }
 
+async function injectWithLogin(this: FastifyInstance, username: string, opts: InjectOptions) {
+  opts.headers = {
+    ...opts.headers,
+    Authorization: `Bearer ${await this.login(username)}`
+  };
+
+  return this.inject(opts);
+};
+
 // automatically build and tear down our instance
 export async function build(t: TestContext) {
   // you can set all the options supported by the fastify CLI command
@@ -36,18 +55,11 @@ export async function build(t: TestContext) {
   // fastify-plugin ensures that all decorators
   // are exposed for testing purposes, this is
   // different from the production setup
-  const app = await buildApplication(argv, config());
+  const app = await buildApplication(argv, config()) as FastifyInstance;
 
   app.login = login;
 
-  app.injectWithLogin = async (username: string, opts: InjectOptions) => {
-    opts.headers = {
-      ...opts.headers,
-      Authorization: `Bearer ${await app.login(username)}`
-    };
-
-    return app.inject(opts);
-  };
+  app.injectWithLogin = injectWithLogin
 
   // close the app after we are done
   t.after(() => app.close());
