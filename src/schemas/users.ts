b74f54c709275c23a025fb05e8ff59bae319f540
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-10-30 19:08:55 +0100

    chore(schemas): update password validation pattern

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -1,6 +1,18 @@
 import { Type } from '@sinclair/typebox'
 
+const atLeastOneUpperCasePattern = '(?=.*?[A-Z])'
+const atLeastOneLowerCasePattern = '(?=.*?[a-z])'
+const atLeastOneNumericPattern = '(?=.*?[0-9])'
+const atLeastOneSpecialCharPattern = '(?=.*?[#?!@$%^&*-])'
+
+const passwordPattern = `^${atLeastOneUpperCasePattern}${atLeastOneLowerCasePattern}${atLeastOneNumericPattern}${atLeastOneSpecialCharPattern}$`
+
+const Password = Type.String({
+  pattern: passwordPattern,
+  minLength: 8
+})
+
 export const UpdateCredentialsSchema = Type.Object({
-  currentPassword: Type.String(),
-  newPassword: Type.String()
+  currentPassword: Password,
+  newPassword: Password
 })
