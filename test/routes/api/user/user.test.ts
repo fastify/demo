b1d46b06b4d061df3a18937512e0756a14c5f408
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-10-31 19:44:23 +0100

    feat: remove comment

--- test/routes/api/user/user.test.ts
+++ test/routes/api/user/user.test.ts
@@ -6,6 +6,22 @@ test('Should return 401 if user does not exist in the database', async (t) => {
   // I should remove the user from the database ?
 })
 
+test('Should update the password successfully', async (t) => {
+  const app = await build(t)
+
+  const res = await app.injectWithLogin('basic', {
+    method: 'PUT',
+    url: '/api/user/update-password',
+    payload: {
+      currentPassword: 'Password123$',
+      newPassword: 'NewPassword123$'
+    }
+  })
+
+  assert.strictEqual(res.statusCode, 200)
+  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Password updated successfully' })
+})
+
 test('Should return 400 if the new password is the same as current password', async (t) => {
   const app = await build(t)
 
@@ -53,118 +69,3 @@ test('Should return 401 the current password is incorrect', async (t) => {
   assert.strictEqual(res.statusCode, 401)
   assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Invalid current password.' })
 })
-
-test('Should update the password successfully', async (t) => {
-  const app = await build(t)
-
-  const res = await app.injectWithLogin('basic', {
-    method: 'PUT',
-    url: '/api/user/update-password',
-    payload: {
-      currentPassword: 'Password123$',
-      newPassword: 'NewPassword123$'
-    }
-  })
-
-  assert.strictEqual(res.statusCode, 200)
-  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Password updated successfully' })
-})
-
-// test('Transaction should rollback on error', async (t) => {
-//   const app = await build(t)
-// const { mock: mockCompare } = t.mock.method(app, 'compare')
-//
-// mockCompare.mockImplementationOnce(() => {
-//   return new Promise((resolve) => {
-//     resolve(true)
-//   })
-// })
-
-// const res = await app.injectWithLogin('basic', {
-//   method: 'PUT',
-//   url: '/api/user/update-password',
-//   payload: {
-//     currentPassword: 'Password123$',
-//     newPassword: 'Xy9#Lm3$'
-//   }
-// })
-// assert.strictEqual(res.statusCode, 200)
-// await app.inject({
-//   method: 'PUT',
-//   url: '/api/user/update-password',
-//   payload: {
-//     newPassword: 'password123$',
-//     currentPassword: 'password123$'
-//   }
-// })
-
-// assert.strictEqual(mockCompare.callCount(), 1)
-
-// const arg = mockLogError.calls[0].arguments[0] as unknown as {
-//   err: Error
-// }
-// console.log('%c LOG arg.err.message', 'background: #222; color: #bada55', arg.err.message)
-// assert.strictEqual(res.statusCode, 500)
-// assert.deepStrictEqual(arg.err.message, 'An error occurred while updating the password.')
-// })
-
-// test('POST /api/auth/login with valid credentials', async (t) => {
-//   const app = await build(t)
-//
-//   const res = await app.inject({
-//     method: 'POST',
-//     url: '/api/auth/login',
-//     payload: {
-//       username: 'basic',
-//       password: 'password123$'
-//     }
-//   })
-//
-//   assert.strictEqual(res.statusCode, 200)
-//   assert.ok(
-//     res.cookies.some((cookie) => cookie.name === app.config.COOKIE_NAME)
-//   )
-// })
-//
-// test('POST /api/auth/login with invalid credentials', async (t) => {
-//   const app = await build(t)
-//
-//   const testCases = [
-//     {
-//       username: 'invalid_user',
-//       password: 'password',
-//       description: 'invalid username'
-//     },
-//     {
-//       username: 'basic',
-//       password: 'wrong_password',
-//       description: 'invalid password'
-//     },
-//     {
-//       username: 'invalid_user',
-//       password: 'wrong_password',
-//       description: 'both invalid'
-//     }
-//   ]
-//
-//   for (const testCase of testCases) {
-//     const res = await app.inject({
-//       method: 'POST',
-//       url: '/api/auth/login',
-//       payload: {
-//         username: testCase.username,
-//         password: testCase.password
-//       }
-//     })
-//
-//     assert.strictEqual(
-//       res.statusCode,
-//       401,
-//       `Failed for case: ${testCase.description}`
-//     )
-//
-//     assert.deepStrictEqual(JSON.parse(res.payload), {
-//       message: 'Invalid username or password.'
-//     })
-//   }
-// })
