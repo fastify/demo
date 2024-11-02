bb09fdb26aa841f1df0627370524e368d8f0d5fc
El Hilali Mouad <elhilali.mad@gmail.com>
2024-11-02 16:18:51 +0100

    refactor(file-handling): explicit binding by factory & type interface by inference

--- src/plugins/custom/file-handling.ts
+++ src/plugins/custom/file-handling.ts
@@ -7,10 +7,7 @@ import fs from 'node:fs'
 
 declare module 'fastify' {
   export interface FastifyInstance {
-    fileHandler: {
-      upload: (fileDir: string, fileName: string, file: BusboyFileStream) => Promise<void>
-      remove: (fileDir: string, fileName: string) => Promise<void>
-    }
+    fileHandler: ReturnType<typeof fileHandlerFactory>
   }
 }
 
@@ -24,35 +21,47 @@ function buildFilePath (fastify: FastifyInstance, fileDir: string, filename: str
   )
 }
 
-async function upload (this: FastifyInstance, fileDir: string, fileName: string, file: BusboyFileStream) {
-  const filePath = buildFilePath(this, fileName, fileDir)
+function fileHandlerFactory(fastify: FastifyInstance) {
+  async function upload (fileDir: string, fileName: string, file: BusboyFileStream) {
+    console.log('here')
 
-  await pipeline(file, fs.createWriteStream(filePath))
-}
+    const filePath = buildFilePath(fastify, fileDir, fileName)
 
-async function remove (this: FastifyInstance, fileDir: string, fileName: string) {
-  const filePath = buildFilePath(this, fileName, fileDir)
+    console.log(filePath)
 
-  try {
-    await fs.promises.unlink(filePath)
-  } catch (err) {
-    if (isErrnoException(err) && err.code === 'ENOENT') {
-      // A file could have been deleted by an external actor, e.g. system administrator.
-      // We log the error to keep a record of the failure, but consider that the operation was successful.
-      this.log.warn(`File path '${fileName}' not found`)
-    } else {
-      throw err
+    await pipeline(file, fs.createWriteStream(filePath))
+  }
+
+  async function remove (fileDir: string, fileName: string) {
+    const filePath = buildFilePath(fastify, fileDir, fileName)
+
+    try {
+      await fs.promises.unlink(filePath)
+    } catch (err) {
+      if (isErrnoException(err) && err.code === 'ENOENT') {
+        // A file could have been deleted by an external actor, e.g. system administrator.
+        // We log the error to keep a record of the failure, but consider that the operation was successful.
+        fastify.log.warn(`File path '${fileName}' not found`)
+      } else {
+        throw err
+      }
     }
   }
+
+  return {
+    upload,
+    remove
+  }
 }
+
+
 
 function isErrnoException (error: unknown): error is NodeJS.ErrnoException {
   return error instanceof Error && 'code' in error
 }
 
 export default fp(async (fastify) => {
-  fastify.decorate('fileHandler', {
-    upload: upload.bind(fastify),
-    remove: remove.bind(fastify)
-  })
+
+
+  fastify.decorate('fileHandler', fileHandlerFactory(fastify))
 }, { name: 'file-handler' })
