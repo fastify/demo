aeb54145c99850f64c44b471b1e7d3b06a239bf9
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-11-06 05:55:08 +0100

    refactor: simplify password pattern regex
    - Simplify the password pattern regex to require at least one uppercase, one lowercase, one numeric, and one special character

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -1,11 +1,7 @@
 import { Type } from '@sinclair/typebox'
 
-const atLeastOneUpperCasePattern = '(?=.*?[A-Z])'
-const atLeastOneLowerCasePattern = '(?=.*?[a-z])'
-const atLeastOneNumericPattern = '(?=.*?[0-9])'
-const atLeastOneSpecialCharPattern = '(?=.*?[#?!@$%^&*-])'
+const passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$'
 
-const passwordPattern = `^${atLeastOneUpperCasePattern}${atLeastOneLowerCasePattern}${atLeastOneNumericPattern}${atLeastOneSpecialCharPattern}.*$`
 const Password = Type.String({
   pattern: passwordPattern,
   minLength: 8
