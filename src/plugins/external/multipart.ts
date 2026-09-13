936ea82d0f2ffd276e2638afd4bbd5d8ca86c108
Aaron Finn <aaron.finn@thebodycoach.com>
2026-09-13 19:14:38 +0100

    correct code comment for fileSize (#164)

--- src/plugins/external/multipart.ts
+++ src/plugins/external/multipart.ts
@@ -5,7 +5,7 @@ export const autoConfig = {
     fieldNameSize: 100, // Max field name size in bytes
     fieldSize: 100, // Max field value size in bytes
     fields: 10, // Max number of non-file fields
-    fileSize: 1 * 1024 * 1024, // Max file size in bytes (5 MB)
+    fileSize: 1 * 1024 * 1024, // Max file size in bytes (1 MB)
     files: 1, // Max number of file fields
     parts: 1000 // Max number of parts
   }
