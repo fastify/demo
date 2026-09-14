f4f60bc45f2fc9ce0bcd3405c9b0fd5c7a0f37cd
Aaron Finn <aaron.finn@thebodycoach.com>
2026-09-14 10:18:04 +0100

    remove unnecessary comment

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,3 @@
-/**
- * This plugin is registered by src/server.ts, the app's actual entry point.
- */
-
 import path from 'node:path'
 import fastifyAutoload from '@fastify/autoload'
 import { FastifyError, FastifyInstance, FastifyPluginOptions } from 'fastify'
